d0a2711a079f4de1a6628bc8f2011a8a79e932ba
Anton Oshurek <oshurekdev@proton.me>
2025-01-26 20:17:08 +0100

    add icons for bottom navigation

--- src/app/navigation/bottom-tabs.tsx
+++ src/app/navigation/bottom-tabs.tsx
@@ -4,6 +4,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HomeScreen, InfoScreen, SettingsScreen, GroupsScreen } from '../../screens';
 //TYPES
 import { BottomTabsParams, BottomTabsNavigationParams } from './model/bottom-tabs.model';
+//ICONS
+import { HomeIcon, SettingsIcon, ListIcon, InfoIcon } from '../../shaared/icons/';
 
 const BottomTabs = (): JSX.Element => {
 	const BottomTabs = createBottomTabNavigator<BottomTabsNavigationParams>();
@@ -13,22 +15,34 @@ const BottomTabs = (): JSX.Element => {
 			<BottomTabs.Screen
 				component={HomeScreen}
 				name={BottomTabsParams.HomeScreen}
-				options={{ title: 'Home' }}
+				options={{
+					title: '',
+					tabBarIcon: ({ color }) => <HomeIcon height={40} width={40} />
+				}}
 			/>
 			<BottomTabs.Screen
 				component={GroupsScreen}
 				name={BottomTabsParams.Groups}
-				options={{ title: 'Groups' }}
+				options={{
+					title: '',
+					tabBarIcon: ({ color }) => <ListIcon height={40} width={40} />
+				}}
 			/>
 			<BottomTabs.Screen
 				component={SettingsScreen}
 				name={BottomTabsParams.SettingsScreen}
-				options={{ title: 'Settings' }}
+				options={{
+					title: '',
+					tabBarIcon: ({ color }) => <SettingsIcon height={40} width={40} />
+				}}
 			/>
 			<BottomTabs.Screen
 				component={InfoScreen}
 				name={BottomTabsParams.InfoScreen}
-				options={{ title: 'Info' }}
+				options={{
+					title: '',
+					tabBarIcon: ({ color }) => <InfoIcon height={40} width={40} />
+				}}
 			/>
 		</BottomTabs.Navigator>
 	);
--- src/shaared/icons/index.ts
+++ src/shaared/icons/index.ts
@@ -1,3 +1,7 @@
 import PlusIcon from './plus-icon';
+import HomeIcon from './home-icon';
+import SettingsIcon from './settings-icon';
+import InfoIcon from './info-icon';
+import ListIcon from './list-icon';
 
-export { PlusIcon };
+export { PlusIcon, HomeIcon, SettingsIcon, InfoIcon, ListIcon };
