68bb59a5e766838ceec47dc511a550e7dffc8ac4
Anton Oshurek <oshurekdev@proton.me>
2025-01-26 20:00:40 +0100

    navigation refactoring

--- src/app/navigation/stack.tsx
+++ src/app/navigation/stack.tsx
@@ -1,19 +1,18 @@
 //NAVIGATION
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CounterTopTabs from './counter-top-tabs';
-//SCREENS
-import { NewCounterScreen } from '../../screens';
+import BottomTabs from './bottom-tabs';
 //TYPES
-import { StackProps, NavigationStackParams, StackParams } from './model/stack.model';
+import { NavigationStackParams, StackParams } from './model/stack.model';
 
-const Stack = ({ BottomTabsNavigation }: StackProps): JSX.Element => {
+const Stack = (): JSX.Element => {
 	const Stack = createNativeStackNavigator<NavigationStackParams>();
 
 	return (
 		<Stack.Navigator initialRouteName={StackParams.BottomTabs} screenOptions={{}}>
 			<Stack.Screen
 				name={StackParams.BottomTabs}
-				component={BottomTabsNavigation}
+				component={BottomTabs}
 				options={{
 					title: 'Home',
 					headerShown: false
@@ -22,7 +21,7 @@ const Stack = ({ BottomTabsNavigation }: StackProps): JSX.Element => {
 			<Stack.Screen
 				name={StackParams.NewCounterScreen}
 				component={CounterTopTabs}
-				options={{ title: 'New counter' }}
+				options={{ title: 'Counter' }}
 			/>
 		</Stack.Navigator>
 	);
