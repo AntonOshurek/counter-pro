a5866b47ba61886f4d103c6f5713f724b2054d1f
Anton Oshurek <oshurekdev@proton.me>
2025-01-28 21:07:41 +0100

    create basic methods for async storage

--- src/services/async-store-service/async-store.service.ts
+++ src/services/async-store-service/async-store.service.ts
@@ -1,9 +1,38 @@
+//ASYNC STORAGE
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 class AsyncStorageService {
-	getItem<T>(itemName: string): T | Error | null {
-		return null;
+	async getItem<T>(itemName: string): Promise<T | Error | null> {
+		try {
+			const jsonValue: string | null = await AsyncStorage.getItem(itemName);
+
+			if (jsonValue) {
+				const parsedData: T = JSON.parse(jsonValue);
+				return parsedData;
+			} else {
+				return null;
+			}
+		} catch (error) {
+			if (error instanceof Error) {
+				return new Error(error.message);
+			} else {
+				return new Error('Failed to get data from mobile storage.');
+			}
+		}
 	}
 
-	setItem<T>(itemName: string, payload: T) {}
+	async setItem<T>(itemName: string, payload: T): Promise<Error | void> {
+		try {
+			const jsonValue = JSON.stringify(payload);
+			await AsyncStorage.setItem(itemName, jsonValue);
+		} catch (error) {
+			if (error instanceof Error) {
+				return new Error(error.message);
+			} else {
+				return new Error('Failed to set data to mobile storage.');
+			}
+		}
+	}
 }
 
 const asyncStorageService = new AsyncStorageService();
