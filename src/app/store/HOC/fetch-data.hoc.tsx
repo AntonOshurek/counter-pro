114d9fb216d0a7c11e2ce6081fe3add0d47ff8e4
Anton Oshurek <oshurekdev@proton.me>
2025-01-29 22:54:36 +0100

    create HOC

--- src/app/store/HOC/fetch-data.hoc.tsx
+++ src/app/store/HOC/fetch-data.hoc.tsx
@@ -1,31 +1,34 @@
+//REACT
 import { ComponentType, useEffect } from 'react';
+//STORE
 import { useAppDispatch } from '../hooks/hooks';
-import appRepository from '../../../services/app-repository/app-repository';
 import { updateState } from '../actions/app-actions';
+//REPOSITORY
+import appRepository from '../../../services/app-repository/app-repository';
 
 const FetchDataHoc = <P extends object>(Component: ComponentType<P>) => {
-  const dispatch = useAppDispatch();
+	const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    const fetchState = async () => {
-      try {
-        const state = await appRepository.getState();
+	useEffect(() => {
+		(() => async () => {
+			try {
+				const state = await appRepository.getState();
 
-        if (state instanceof Error) {
-          console.error('Failed to load state:', state.message);
-          return;
-        }
+				if (state instanceof Error) {
+          //dispatch this error
+					console.error('Failed to load state:', state.message);
+					return;
+				}
 
-        dispatch(updateState({newState: state})); // Обновляем Redux
-      } catch (error) {
-        console.error('Unexpected error in fetchState:', error);
-      }
-    };
+				dispatch(updateState({ newState: state }));
+			} catch (error) {
+        //dispatch this error
+				console.error('Unexpected error in fetchState:', error);
+			}
+		})();
+	}, [dispatch]);
 
-    fetchState();
-  }, [dispatch]);
-
-  return (props: P) => {
+	return (props: P) => {
 		return <Component {...props} />;
 	};
 };
