335166c8ccf08a61154b06f27edfdbd4301d0ba5
Anton Oshurek <oshurekdev@proton.me>
2025-02-07 23:12:19 +0100

    change theme color in create button

--- src/shaared/ui/create-button/create-button.tsx
+++ src/shaared/ui/create-button/create-button.tsx
@@ -1,18 +1,23 @@
 //NATIVE
 import { Pressable, Text } from 'react-native';
+//HOOKS
+import UseThemeResolver from '../../hooks/useThemeResolver';
 //TYPES
 import type { CreateButtonProps } from './model/create-button.model';
 //ICONS
 import { PlusIcon } from '../../icons';
 //STYLES
+import { semanticStyle, colors } from '../../styles/';
 import style from './style/style';
-import { semanticStyle } from '../../styles/';
 
-const CreateButton = ({ onPress, hiddenText }: CreateButtonProps): JSX.Element => {
-	return (
-		<Pressable style={style.createButton} onPress={onPress}>
+const CreateButton = ({ onPress, hiddenText }: CreateButtonProps) => {
+	const theme = UseThemeResolver();
+  const s = style(theme);
+
+  return (
+		<Pressable style={s.createButton} onPress={onPress}>
 			<Text style={semanticStyle.visuallyHidden}>{hiddenText}</Text>
-			<PlusIcon width={40} height={40} color='#000000' />
+			<PlusIcon width={40} height={40} color={colors[theme].textPrimary} />
 		</Pressable>
 	);
 };
