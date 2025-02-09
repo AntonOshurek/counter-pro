17ac5079f807a0f8c82d13154123203b15fc62a7
Anton Oshurek <oshurekdev@proton.me>
2025-02-09 14:04:42 +0100

    foo

--- src/shaared/ui/create-button/create-button.tsx
+++ src/shaared/ui/create-button/create-button.tsx
@@ -12,9 +12,9 @@ import style from './style/style';
 
 const CreateButton = ({ onPress, hiddenText }: CreateButtonProps) => {
 	const theme = UseThemeResolver();
-  const s = style(theme);
+	const s = style(theme);
 
-  return (
+	return (
 		<Pressable style={s.createButton} onPress={onPress}>
 			<Text style={semanticStyle.visuallyHidden}>{hiddenText}</Text>
 			<PlusIcon width={40} height={40} color={colors[theme].textPrimary} />
--- src/shaared/ui/create-button/style/style.ts
+++ src/shaared/ui/create-button/style/style.ts
@@ -4,13 +4,14 @@ import type { ColorThemes } from '../../../../entities/theme';
 //COLORS
 import { colors } from '../../../styles';
 
-const style = (theme: ColorThemes) => StyleSheet.create({
-	createButton: {
-    backgroundColor: colors[theme].mainSurfaceTertiary,
-		borderRadius: 100,
-		paddingVertical: 10,
-		paddingHorizontal: 10
-	}
-});
+const style = (theme: ColorThemes) =>
+	StyleSheet.create({
+		createButton: {
+			backgroundColor: colors[theme].mainSurfaceTertiary,
+			borderRadius: 100,
+			paddingVertical: 10,
+			paddingHorizontal: 10
+		}
+	});
 
 export default style;
