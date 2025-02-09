7cd3b42bef23e4e7fbeb471899d216896289247e
Anton Oshurek <oshurekdev@proton.me>
2025-02-09 21:38:13 +0100

    refactoring group page navigation

---
+++ src/app/navigation/model/index.ts
@@ -0,0 +1,19 @@
+import { NavigationStackParams } from './stack.model';
+import { BottomTabsNavigationParams } from './bottom-tabs.model';
+import { CounterTopTabsNavigationParams } from './counter-top-tabs.model';
+import {
+	GroupTopTabsNavigationParams,
+	GroupScreenRouteProp,
+	GroupScreenNavigationProp,
+	GroupTopTabsProps
+} from './group-top-tabs.model';
+
+export {
+	NavigationStackParams,
+	BottomTabsNavigationParams,
+	CounterTopTabsNavigationParams,
+	GroupTopTabsNavigationParams,
+	GroupScreenRouteProp,
+	GroupScreenNavigationProp,
+	GroupTopTabsProps
+};
