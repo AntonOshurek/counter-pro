53588009ea764a9d9f64e9448f2b85b33c238546
Anton Oshurek <oshurekdev@proton.me>
2025-02-14 20:04:38 +0100

    move groupslist feature to groups widget

--- src/widgets/groups/groups.widget.tsx
+++ src/widgets/groups/groups.widget.tsx
@@ -1,14 +1,23 @@
 //NATIVE
-import { View } from 'react-native';
+import { FlatList, View } from 'react-native';
 //FEATURES
-import { GroupsList } from '../../features';
+
+import { GroupCard } from '../../entities/group';
 //STYLES
 import style from './styles/style';
+
+//MOK DATA
+import groups from '../../mok-data/groups';
 
 const GroupsWidget = () => {
 	return (
 		<View style={style.groups}>
-			<GroupsList />
+      <FlatList
+        style={style.groupsList}
+        data={groups}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <GroupCard item={item} key={item.id} />}
+      />
 		</View>
 	);
 };
--- src/widgets/groups/styles/style.ts
+++ src/widgets/groups/styles/style.ts
@@ -3,7 +3,11 @@ import { StyleSheet } from 'react-native';
 const style = StyleSheet.create({
 	groups: {
 		flex: 1
-	}
+	},
+  groupsList: {
+    paddingInline: 20,
+    paddingTop: 20
+  }
 });
 
 export default style;
