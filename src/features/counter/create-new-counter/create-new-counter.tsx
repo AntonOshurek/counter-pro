d4e132fb41765e17ce0348089ca29c8892263964
Anton Oshurek <oshurekdev@proton.me>
2025-02-21 21:31:10 +0100

    basic logick for create new counter feature

--- src/features/counter/create-new-counter/create-new-counter.tsx
+++ src/features/counter/create-new-counter/create-new-counter.tsx
@@ -2,14 +2,32 @@
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { NavigationStackParams } from '../../../app/navigation/model';
+//STORE
+import { useAppDispatch } from '../../../shared/store';
+import { CreateCounterAction } from '../../../entities/counter';
 //CONSTANTS
 import { StackScreens } from '../../../shared/constants';
+//LIBS
+import { v4 } from 'uuid';
 
 const useCreateNewCounter = () => {
 	const navigation = useNavigation<NativeStackNavigationProp<NavigationStackParams>>();
 
+	const dispatch = useAppDispatch();
+
 	return () => {
-		navigation.navigate(StackScreens.CounterScreen);
+		const newCounter = {
+			id: v4(),
+			name: 'New Counter',
+			step: 1,
+			count: 0,
+			group: ''
+		};
+
+		console.log('create counter', newCounter);
+
+		dispatch(CreateCounterAction({ newCounter: newCounter }));
+		navigation.navigate(StackScreens.CounterScreen, { counter: newCounter });
 	};
 };
 
