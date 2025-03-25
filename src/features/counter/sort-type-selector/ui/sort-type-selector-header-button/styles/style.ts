88d57d16aa971a4e4c4c3486cc73ac39fedcd545
Anton Oshurek <oshurekdev@proton.me>
2025-03-25 19:24:17 +0100

    refactoring

--- src/features/counter/sort-type-selector/ui/sort-type-selector-header-button/styles/style.ts
+++ src/features/counter/sort-type-selector/ui/sort-type-selector-header-button/styles/style.ts
@@ -5,28 +5,6 @@ import type { ColorThemes } from '../../../../../../entities/theme';
 //STYLES
 import { colors } from '../../../../../../shared/styles';
 
-const style = (theme: ColorThemes) =>
-	StyleSheet.create({
-		overlay: {
-			flex: 1,
-			backgroundColor: 'rgba(0, 0, 0, 0.5)'
-		},
-		menu: {
-			position: 'absolute',
-			top: 50,
-			right: 10,
-			backgroundColor: colors[theme].mainSurfaceSecondary,
-			borderRadius: 5,
-			elevation: 5
-		},
-		item: {
-			borderRadius: 5,
-			paddingInline: 20,
-			paddingVertical: 10
-		},
-		selected: {
-			backgroundColor: colors[theme].mainSurfaceTertiary
-		}
-	});
+const style = (theme: ColorThemes) => StyleSheet.create({});
 
 export default style;
