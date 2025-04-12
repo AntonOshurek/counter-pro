ef2cdec67406c8d1b50915ebfa73916c88bb4bc4
Anton Oshurek <oshurekdev@proton.me>
2025-04-12 10:03:43 +0200

    refactor

--- src/widgets/group-settings/group-settings.widget.tsx
+++ src/widgets/group-settings/group-settings.widget.tsx
@@ -2,7 +2,6 @@
 import { ChangeNameField } from '../../features/group';
 //UI
 import { SettingsGroupWrapper } from '../../shared/wrappers';
-import Paragraph from '../../shared/ui/paragraph/paragraph';
 //MODEL
 import { GroupSettingsWidgetProps } from './model/group-settings-widget.model';
 //STYLES
@@ -11,10 +10,6 @@ import style from './styles/style';
 const GroupSettingsWidget = ({ group }: GroupSettingsWidgetProps) => {
 	return (
 		<SettingsGroupWrapper additionalClass={style.groupSettings}>
-			<Paragraph contentType={'primary'} size={'medium'}>
-				{group.name}
-			</Paragraph>
-
 			<ChangeNameField groupId={group.id} groupName={group.name} />
 		</SettingsGroupWrapper>
 	);
