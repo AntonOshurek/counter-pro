20580b0e4b4319d3c649923ad5c2d4dc6395b3ab
Anton Oshurek <oshurekdev@proton.me>
2025-04-14 19:49:57 +0200

    modify modal position and overlay

--- src/shared/ui/checkbox-modal/checkbox-modal.tsx
+++ src/shared/ui/checkbox-modal/checkbox-modal.tsx
@@ -29,39 +29,40 @@ const CheckboxModal = ({
 
 	return (
 		<Modal visible={visible} transparent animationType='slide'>
-			<TouchableOpacity style={s.overlay} onPress={onClose}>
-				<View style={s.container}>
-					<Paragraph size={'large'} contentType={'primary'}>
-						{title}
+			{/*<View style={s.modalWrapper}>*/}
+			<View style={s.container}>
+				<Paragraph size={'large'} contentType={'primary'}>
+					{title}
+				</Paragraph>
+
+				{items.length > 0 ? (
+					<FlatList
+						data={items}
+						keyExtractor={item => item.id}
+						renderItem={({ item }) => {
+							return (
+								<TouchableOpacity style={s.item} onPress={() => checkboxToggle(item)}>
+									<CustomCheckbox
+										value={item.isSelected}
+										onChange={() => checkboxToggle(item)}
+									/>
+									<Paragraph size={'medium'} contentType={'primary'}>
+										{item.label}
+									</Paragraph>
+								</TouchableOpacity>
+							);
+						}}
+					/>
+				) : (
+					<Paragraph contentType={'tertiary'} size={'small'}>
+						{itemsIsEmptyText}
 					</Paragraph>
+				)}
 
-					{items.length > 0 ? (
-						<FlatList
-							data={items}
-							keyExtractor={item => item.id}
-							renderItem={({ item }) => {
-								return (
-									<TouchableOpacity style={s.item} onPress={() => checkboxToggle(item)}>
-										<CustomCheckbox
-											value={item.isSelected}
-											onChange={() => checkboxToggle(item)}
-										/>
-										<Paragraph size={'medium'} contentType={'primary'}>
-											{item.label}
-										</Paragraph>
-									</TouchableOpacity>
-								);
-							}}
-						/>
-					) : (
-						<Paragraph contentType={'tertiary'} size={'small'}>
-							{itemsIsEmptyText}
-						</Paragraph>
-					)}
-
-					<MainButton onPress={onClose} label={'Close modal'} />
-				</View>
-			</TouchableOpacity>
+				<MainButton onPress={onClose} label={'Close modal'} />
+			</View>
+			{/*</View>*/}
+			<TouchableOpacity style={s.overlay} onPress={onClose}></TouchableOpacity>
 		</Modal>
 	);
 };
--- src/shared/ui/checkbox-modal/styles/style.ts
+++ src/shared/ui/checkbox-modal/styles/style.ts
@@ -1,12 +1,15 @@
 //NATIVE
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 //MODEL
 import type { ColorThemes } from '../../../../entities/theme';
 //STYLES
 import { colors } from '../../../styles';
 
+const { width, height } = Dimensions.get('window');
+
 const style = (theme: ColorThemes) =>
 	StyleSheet.create({
+		modalWrapper: {},
 		overlay: {
 			flex: 1,
 			backgroundColor: '#00000070',
@@ -14,6 +17,14 @@ const style = (theme: ColorThemes) =>
 			alignItems: 'center'
 		},
 		container: {
+			position: 'absolute',
+			top: '50%',
+			left: '50%',
+			transform: [
+				{ translateX: -(width * 0.9) / 2 },
+				{ translateY: -(height * 0.3) / 2 }
+			],
+			zIndex: 1,
 			width: '90%',
 			maxHeight: '70%',
 			backgroundColor: colors[theme].mainSurfaceTertiary,
