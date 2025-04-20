7ecd2d9d1bbbf21c04e444e36804716cf366ccad
Anton Oshurek <oshurekdev@proton.me>
2025-04-20 14:04:53 +0200

    implement abstract class for repo

---
+++ src/services/app-repository/abstract-repository.ts
@@ -0,0 +1,60 @@
+import asyncStorageService from '@services/async-store-service/async-store.service';
+
+abstract class AbstractRepository<T> {
+	#defaultState: T;
+	#storeName: string;
+
+	protected constructor(initialState: T, storeName: string) {
+		this.#defaultState = initialState;
+		this.#storeName = storeName;
+	}
+
+	async #getStateFromStorage() {
+		return await asyncStorageService.getItem<T>(this.#storeName);
+	}
+
+	async #sendStateToStorage(data: T) {
+		return await asyncStorageService.setItem<T>(this.#storeName, data);
+	}
+
+	#getDefaultState(): T {
+		return this.#defaultState;
+	}
+
+	async getState(): Promise<T | Error> {
+		try {
+			const state = await this.#getStateFromStorage();
+
+			if (state === null) {
+				return this.#getDefaultState();
+			}
+
+			return state;
+		} catch (error) {
+			if (error instanceof Error) {
+				return new Error(error.message);
+			} else {
+				return new Error(
+					'Failed to set data to mobile storage. Please restart Your application and try again.'
+				);
+			}
+		}
+	}
+
+	async setState(newState: T): Promise<boolean | Error> {
+		try {
+			await this.#sendStateToStorage(newState);
+			return true;
+		} catch (error) {
+			if (error instanceof Error) {
+				return new Error(error.message);
+			} else {
+				return new Error(
+					'Failed to set data to mobile storage. Please restart Your application and try again.'
+				);
+			}
+		}
+	}
+}
+
+export default AbstractRepository;
--- src/services/app-repository/app-repository.ts
+++ src/services/app-repository/app-repository.ts
@@ -1,67 +1,16 @@
-//SERVICES
-import asyncStorageService from '../async-store-service/async-store.service';
 //STATE
 import { appInitialState } from '@entities/app/store/state/app-state';
 //TYPES
 import type { IAppState } from '@entities/app/';
+//ABSTRACT
+import AbstractRepository from '@services/app-repository/abstract-repository';
 
-class AppRepository {
-	#defaultAppState: IAppState;
-	#appStoreName: string;
-
-	constructor(initialState: IAppState, appStoreName: string) {
-		this.#defaultAppState = initialState;
-		this.#appStoreName = appStoreName;
-	}
-
-	async #getStateFromStorage() {
-		return await asyncStorageService.getItem<IAppState>(this.#appStoreName);
-	}
-
-	async #sendStateToStorage(data: IAppState) {
-		return await asyncStorageService.setItem<IAppState>(this.#appStoreName, data);
-	}
-
-	#getDefaultState(): IAppState {
-		return this.#defaultAppState;
-	}
-
-	async getState(): Promise<IAppState | Error> {
-		try {
-			const state = await this.#getStateFromStorage();
-
-			if (state === null) {
-				return this.#getDefaultState();
-			}
-
-			return state;
-		} catch (error) {
-			if (error instanceof Error) {
-				return new Error(error.message);
-			} else {
-				return new Error(
-					'Failed to set data to mobile storage. Please restart Your application and try again.'
-				);
-			}
-		}
-	}
-
-	async setState(newState: IAppState): Promise<boolean | Error> {
-		try {
-			await this.#sendStateToStorage(newState);
-			return true;
-		} catch (error) {
-			if (error instanceof Error) {
-				return new Error(error.message);
-			} else {
-				return new Error(
-					'Failed to set data to mobile storage. Please restart Your application and try again.'
-				);
-			}
-		}
+export class AppRepository extends AbstractRepository<IAppState> {
+	constructor() {
+		super(appInitialState, 'app-store');
 	}
 }
 
-const appRepository = new AppRepository(appInitialState, 'app-store');
+const appRepository = new AppRepository();
 
 export default appRepository;
