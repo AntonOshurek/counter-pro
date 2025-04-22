720a3171d03d75192bc7af88cb08a1677451956d
Anton Oshurek <oshurekdev@proton.me>
2025-04-22 22:28:47 +0200

    move sql text to separate file

--- src/entities/counter/db/init-counter-table.ts
+++ src/entities/counter/db/init-counter-table.ts
@@ -1,19 +1,9 @@
 //DB
 import { SQLiteDatabase } from 'expo-sqlite';
+import { CREATE_COUNTER_TABLE_SQL } from '@entities/counter/db/sql/create-counter-table.sql';
 
 const initCounterTable = async (db: SQLiteDatabase) => {
-  await db.execAsync(`
-    CREATE TABLE IF NOT EXISTS counters (
-      id TEXT PRIMARY KEY NOT NULL,
-      name TEXT NOT NULL,
-      step INTEGER NOT NULL,
-      count INTEGER NOT NULL,
-      group_name TEXT NOT NULL,
-      created_at TEXT NOT NULL,
-      is_pinned INTEGER NOT NULL,
-      created_at_timestamp INTEGER NOT NULL
-    );
-  `);
+	await db.execAsync(CREATE_COUNTER_TABLE_SQL);
 };
 
 export { initCounterTable };
