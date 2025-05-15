0a4004a0453dc6b9bfb846b38915b16a372cf1e2
Anton Oshurek <oshurekdev@proton.me>
2025-05-15 23:03:42 +0200

    create separate layout for set bg theme to app

--- src/app/navigation/navigation.tsx
+++ src/app/navigation/navigation.tsx
@@ -1,19 +1,12 @@
 //NAVIGATION
 import { NavigationContainer } from '@react-navigation/native';
 import Stack from './stack';
-import { colors } from '@shared/styles';
-import useThemeResolver from '@shared/hooks/useThemeResolver';
-import { View } from 'react-native';
 
 const Navigation = () => {
-	const theme = useThemeResolver();
-
 	return (
-		<View style={{ flex: 1, backgroundColor: colors[theme].mainSurfacePrimary }}>
-			<NavigationContainer>
-				<Stack />
-			</NavigationContainer>
-		</View>
+		<NavigationContainer>
+			<Stack />
+		</NavigationContainer>
 	);
 };
 
