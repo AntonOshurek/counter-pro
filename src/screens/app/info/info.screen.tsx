0bdb3d5c6738f08ac5646b9c89f98951e3437f3b
Anton Oshurek <oshurekdev@proton.me>
2025-05-22 20:22:25 +0200

    change info screen and add social media icons and links

--- src/screens/app/info/info.screen.tsx
+++ src/screens/app/info/info.screen.tsx
@@ -1,27 +1,42 @@
 //NATIVE
-import { View } from 'react-native';
+import { Linking, Pressable, View } from 'react-native';
 //LAYOUTS
 import { ScreenLayout } from '@shared/layouts/';
 //UI
 import { Paragraph } from '@shared/ui';
 //STYLES
 import style from './styles/style';
+import { GitHubIcon, LinkedinIcon } from '@shared/icons';
 
 const InfoScreen = () => {
 	return (
 		<ScreenLayout additionalClass={style.infoScreen}>
 			<View style={style.about}>
 				<Paragraph size={'large'} contentType={'primary'}>
-					This application created by AADev
+					Created by Anton Oshurek
 				</Paragraph>
+				<View style={style.socialLinks}>
+					<Pressable
+						onPress={() => Linking.openURL('https://github.com/AntonOshurek')}
+						style={style.link}
+					>
+						<GitHubIcon width={40} height={40} />
+					</Pressable>
+					<Pressable
+						onPress={() => Linking.openURL('https://www.linkedin.com/in/anton-oshurek/')}
+						style={style.link}
+					>
+						<LinkedinIcon width={40} height={40} />
+					</Pressable>
+				</View>
 			</View>
 
 			<View style={style.releaseInfo}>
 				<Paragraph size={'small'} contentType={'secondary'}>
-					App version - 0.0.1
+					App version - 0.9.0
 				</Paragraph>
 				<Paragraph size={'small'} contentType={'secondary'}>
-					Release date - 27.01.2025
+					Release date - 22.05.2025
 				</Paragraph>
 			</View>
 		</ScreenLayout>
